1fcae42418f6627f1a02108dc5e7ea959497d8ba
Who? <who@Whos-MacBook-Air.local>
2021-08-10 17:52:12 -0600

    commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+// Packages
+import React from 'react';
+//
+
+// Components
+import Header from './components/Header';
+import Footer from './components/Footer';
+//
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
